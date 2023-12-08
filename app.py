b8213ceaab93dd442e5b55d8464f233a3f58a7d6
kou7306 <kouta.1206.y@icloud.com>
2023-12-08 23:08:03 +0900

    deploy

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 # main.py
 from flask import Flask, render_template
+from dotenv import load_dotenv
 import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    api_key = os.getenv('GOOGLE_MAPS_API_KEY')
+    api_key = os.environ['GOOGLE_MAPS_API_KEY']
     return render_template('index.html', api_key=api_key)
 
 if __name__ == '__main__':
