503c1ab674948d8a3c23eb85f36a236a182f9f62
kou7306 <kouta.1206.y@icloud.com>
2023-12-10 14:44:15 +0900

    deploy error

--- static/js/script.js
+++ static/js/script.js
@@ -9,18 +9,10 @@ liff.init({
 
 function runApp() {
     liff.getProfile().then(profile => {
-        const userName = profile.displayName;
-
-        liff.getGeolocation()
-            .then(location => {
-                const userLocationText = `Latitude: ${location.latitude}, Longitude: ${location.longitude}`;
-
-                // Display user information on the page
-                document.getElementById('userName').innerText = `User: ${userName}`;
-                document.getElementById('userLocation').innerText = `Location: ${userLocationText}`;
-            })
-            .catch(error => {
-                console.error('Error getting location', error);
-            });
-    });
-}
+            const userName = profile.displayName;
+            document.getElementById('userName').innerText = `User: ${userName}`;
+        })
+        .catch(error => {
+            console.error('Error getting location', error);
+        });
+    }
