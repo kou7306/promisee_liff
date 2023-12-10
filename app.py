6654a3b3a17cb75e8f007302094784134e2583b9
kou7306 <kouta.1206.y@icloud.com>
2023-12-10 15:28:21 +0900

    data

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 # main.py
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/submit_response',methods={"POST"})
+def submit():
+    user_name = request.form.get('user_name')
+    response_type = request.form.get('response_type')
+
+    new_response = Response(user_name=user_name, response_type=response_type)
+    db.session.add(new_response)
+    db.session.commit()
+
+    return 'Response submitted successfully!'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,36 @@
 liff.init({
     liffId: '2002096181-Ryql27BY', // Use own liffId
     withLoginOnExternalBrowser: true, // Enable automatic login process
-}).then(() => {
-    // Start to use liff's api
-    runApp();
-});
+})
 
 
-function runApp() {
+
+function submitResponse(responseType) {
     liff.getProfile().then(profile => {
-            const userName = profile.displayName;
-            document.getElementById('userName').innerText = `User: ${userName}`;
+        const userName = profile.displayName;
+
+            // Send data to Flask backend
+        fetch('/submit_response', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                'user_name': userName,
+                'response_type': responseType,
+            }),
+        })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+            if (data.result) {
+                // If the result is true, close the LIFF window
+                liff.closeWindow();
+            } 
+                // Handle the response from the server
         })
         .catch(error => {
-            console.error('Error getting location', error);
+            console.error('Error submitting response', error);
         });
-    }
+    });
+}
